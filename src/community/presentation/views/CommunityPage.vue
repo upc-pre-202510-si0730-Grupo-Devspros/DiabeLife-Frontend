f463aa97e53e2907ab8ac9674ca44c3a25a7b8fa
Gabriel Mamani <u202220659@upc.edu.pe>
2025-10-06 14:21:23 -0500

    feat: add community page component

---
+++ src/community/presentation/views/CommunityPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import PostForm from "../views/PostForm.vue";
+import PostList from "../views/PostList.vue";
+</script>
+
+<template>
+  <div class="p-4 flex flex-column gap-4 max-w-3xl m-auto">
+    <PostForm />
+    <PostList />
+  </div>
+</template>
